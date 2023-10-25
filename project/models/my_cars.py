387f5adf2d1fba5121f3ed105fe8f1ea033a9302
ThereseKH <therese.haugsbo@gmail.com>
2023-10-25 11:46:10 +0200

    Added car for testing

--- project/models/my_cars.py
+++ project/models/my_cars.py
@@ -36,3 +36,12 @@ def delete_car(reg):
     _get_connection().execute_query("MATCH (a:Car{reg: $reg}) delete a;", reg =
 reg)
 
+# Cars added to the sytem for testing
+
+save_car("Ford", "Focus", "11D1234", 2011, 5)
+save_car("Nissan", "Micra", "12D1234", 2012, 5)
+save_car("Toyota", "Yaris", "13D1234", 2013, 5)
+save_car("Ford", "Fiesta", "14D1234", 2014, 5)
+save_car("Volkswagen", "Golf", "15D1234", 2015, 5)
+save_car("Volkswagen", "Polo", "16D1234", 2016, 5)
+save_car("Toyota", "Corolla", "17D1234", 2017, 5)
