47361116014cf22825c815a46b986246223b0a67
SarahUnicorn <you@example.comsarahvetrhus.privat@gmail.com>
2023-10-25 11:20:25 +0200

    my_cars finished!

---
+++ project/models/my_cars.py
@@ -0,0 +1,38 @@
+from neo4j import GraphDatabase, Driver, AsyncGraphDatabase, AsyncDriver
+import json
+URI = "YOUR-URI"
+AUTH = ("neo4j", "YOUR-AUTH-KEY")
+def _get_connection() -> Driver:
+    driver = GraphDatabase.driver(URI, auth=AUTH)
+    driver.verify_connectivity()
+    return driver
+def node_to_json(node):
+    node_properties = dict(node.items())
+    return node_properties
+def findAllCars():
+    with _get_connection().session() as session:
+        cars = session.run("MATCH (a:Car) RETURN a;")
+        nodes_json = [node_to_json(record["a"]) for record in cars]
+        print(nodes_json)
+        return nodes_json
+
+def findCarByReg(reg):
+    with _get_connection().session() as session:
+        cars = session.run("MATCH (a:Car) where a.reg=$reg RETURN a;", reg=reg)
+        print(cars)
+        nodes_json = [node_to_json(record["a"]) for record in cars]
+        print(nodes_json)
+        return nodes_json
+
+
+def save_car(make, model, reg, year, capacity):
+    cars = _get_connection().execute_query("MERGE (a:Car{make: $make, model: $model, reg: $reg, year: $year, capacity:$capacity}) RETURN a;", make = make, model = model, reg = reg, year = year, capacity = capacity)
+    nodes_json = [node_to_json(record["a"]) for record in cars]
+    print(nodes_json)
+    return nodes_json
+
+
+def delete_car(reg):
+    _get_connection().execute_query("MATCH (a:Car{reg: $reg}) delete a;", reg =
+reg)
+
